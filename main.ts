e0dc0d9d3f1952b2562c887babd00261d375f71a
Illia-Dovgalenko <illia.dovgalenko@stu.ocsb.ca>
2026-04-09 21:44:43 -0400

    shows a while loop use perimeter

--- main.ts
+++ main.ts
@@ -1,8 +1,89 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Illia
+ * Created on: Apr 2026
+ * This program shows a while loop use perimeter
 */
 
-basic.showString('Hello, World!')
+// variables
+let sprite: game.LedSprite = null
+let loopCounter = 0
+
+// setup
+basic.clearScreen()
+basic.pause(500)
+basic.showIcon(IconNames.Happy)
+
+// the clockwise perimeter movement
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    sprite = game.createSprite(0, 0)
+
+    loopCounter = 0
+    while (loopCounter < 4) {
+        basic.pause(500)
+        loopCounter = loopCounter + 1
+        sprite.set(LedSpriteProperty.X, loopCounter)
+    }
+
+    loopCounter = 0
+    while (loopCounter < 4) {
+        basic.pause(500)
+        loopCounter = loopCounter + 1
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+    }
+
+    loopCounter = 4
+    while (loopCounter > 0) {
+        basic.pause(500)
+        loopCounter = loopCounter - 1
+        sprite.set(LedSpriteProperty.X, loopCounter)
+    }
+
+    loopCounter = 4
+    while (loopCounter > 0) {
+        basic.pause(500)
+        loopCounter = loopCounter - 1
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+    }
+
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
+
+// the clockwise perimeter movement counter clockwise
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    sprite = game.createSprite(0, 0)
+
+    loopCounter = 0
+    while (loopCounter < 4) {
+        basic.pause(500)
+        loopCounter = loopCounter + 1
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+    }
+
+    loopCounter = 0
+    while (loopCounter < 4) {
+        basic.pause(500)
+        loopCounter = loopCounter + 1
+        sprite.set(LedSpriteProperty.X, loopCounter)
+    }
+
+    loopCounter = 4
+    while (loopCounter > 0) {
+        basic.pause(500)
+        loopCounter = loopCounter - 1
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+    }
+
+    loopCounter = 4
+    while (loopCounter > 0) {
+        basic.pause(500)
+        loopCounter = loopCounter - 1
+        sprite.set(LedSpriteProperty.X, loopCounter)
+    }
+
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
